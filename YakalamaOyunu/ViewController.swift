22b6733dce22f5e1cefafba5b6554bb1ef076160
gursoyasin <gursoyreal@gmail.com>
2023-07-26 14:11:33 +0300

    Initial Commit

---
+++ YakalamaOyunu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YakalamaOyunu
+//
+//  Created by Yasin Gürsoy on 26.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
